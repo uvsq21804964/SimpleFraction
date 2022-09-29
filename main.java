82cd3bcb6c94c9d66f8f6bae1350d4e8ef2d932e
Tom Abbouz <tom.gonnaud@gmail.com>
2022-09-29 22:43:54 +0200

    toString

--- main.java
+++ main.java
@@ -3,6 +3,9 @@ public class main {
     public static void main(String[] args) {
         
         System.out.println("Hello World");
+        fraction f = new fraction();
+        System.out.println(f.toString());
+
     }
     
 }
